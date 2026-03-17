d11aa1c3f456bf340a36c6267bf0ba60c4d5c5e4
Xenofon <75397560+XenofonGk@users.noreply.github.com>
2026-03-16 23:43:52 -0400

    Clean up projectmain.c header comments
    Removed header comments and unnecessary instructions.

--- Project1/projectmain.c
+++ Project1/projectmain.c
@@ -1,19 +1,4 @@
-/***************************************************************** 
 
-    File: projectmain.c
-
-     Author: [Xenofon Gkioka]
-    Seneca email: [xgkioka@myseneca.ca]
-
-    To compile program in codespaces, in terminal pane type:
-        gcc -Wall project.c projectmain.c
-    To run program in codespaces, in terminal pane type:
-        ./a.out
-        
-***************************************************************/
-//your main for the project goes into this file
-//Uncomment the next line if you are using Visual Studio
-//#define _CRT_SECURE_NO_WARNING
 
 #include <stdio.h>
 #include "project.h"
