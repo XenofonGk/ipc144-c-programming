e8a7c4dd5dc31e64801c406fe4222d92a6cf4ef6
Xenofon <75397560+XenofonGk@users.noreply.github.com>
2026-03-16 23:44:15 -0400

    Update projectmain.c

--- Project2/projectmain.c
+++ Project2/projectmain.c
@@ -1,16 +1,3 @@
-    /***********************************************************************
-
-    File: projectmain.c
-
-    Author: [Xenofon Gkioka]
-    Seneca email: [xgkioka@myseneca.ca]
-
-    To compile program in codespaces, in terminal pane type:
-        gcc -Wall project.c projectmain.c
-    To run program in codespaces, in terminal pane type:
-        ./a.out
-
-***********************************************************************/
 
 #include "project.h"
 #include <stdio.h>
