a2917182b1581d1c5a1f373891c19004a81b34a3
Xenofon <75397560+XenofonGk@users.noreply.github.com>
2026-03-16 23:43:45 -0400

    Clean up project.h by removing comments
    Removed unnecessary comments and documentation from project.h

--- Project1/project.h
+++ Project1/project.h
@@ -1,17 +1,3 @@
-/***************************************************************** 
-
-    File: project.h
-
-    Author: [Xenofon Gkioka]
-    Seneca email: [xgkioka@myseneca.ca]
-
-    To compile program in codespaces, in terminal pane type:
-        gcc -Wall project.c projectmain.c
-    To run program in codespaces, in terminal pane type:
-        ./a.out
-        
-***************************************************************/
-//function prototypes go into this file as does any constants
 
 
 int getNumTrainCars(void);
